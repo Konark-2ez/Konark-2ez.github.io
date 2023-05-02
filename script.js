e227b6d54d59b54dc5bc7963ad2fef38f5323b17
Konark-2ez <konarkkumar456@gmail.com>
2023-05-02 23:04:37 +0530

    inspection

--- script.js
+++ script.js
@@ -4,17 +4,17 @@ const resume_btn2 = document.querySelector("#resume-button-2");
 resume_btn1.addEventListener("click", (e) => {
   e.preventDefault();
   window.open(
-    "https://drive.google.com/file/d/13NOplGRKospEeCsnxJYPL8iGWhwKR5h8/view?usp=sharing"
+   // "https://drive.google.com/file/d/13NOplGRKospEeCsnxJYPL8iGWhwKR5h8/view?usp=sharing"
   );
   window.location.href =
-    "https://drive.google.com/uc?export=download&id=13NOplGRKospEeCsnxJYPL8iGWhwKR5h8";
+    //"https://drive.google.com/uc?export=download&id=13NOplGRKospEeCsnxJYPL8iGWhwKR5h8";
 });
 
 resume_btn2.addEventListener("click", (e) => {
   e.preventDefault();
   window.open(
-    "https://drive.google.com/file/d/13NOplGRKospEeCsnxJYPL8iGWhwKR5h8/view?usp=sharing"
+    //"https://drive.google.com/file/d/13NOplGRKospEeCsnxJYPL8iGWhwKR5h8/view?usp=sharing"
   );
   window.location.href =
-    "https://drive.google.com/uc?export=download&id=13NOplGRKospEeCsnxJYPL8iGWhwKR5h8";
+   // "https://drive.google.com/uc?export=download&id=13NOplGRKospEeCsnxJYPL8iGWhwKR5h8";
 });
