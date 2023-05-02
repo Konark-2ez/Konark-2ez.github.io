ee7b1d6b4a42c13728a13b59c50259f3d50fd673
Konark-2ez <konarkkumar456@gmail.com>
2023-05-03 01:29:31 +0530

    test

--- script.js
+++ script.js
@@ -27,6 +27,13 @@ resume_btn3.addEventListener("click", (e) => {
     window.location.href =
       "https://drive.google.com/file/d/1GfU7MINpsxg1hhx5D58duW0aaOvVOCog/view?usp=share_link";
   });
+  document.addEventListener('DOMContentLoaded', function() {
+    const resume_btn2 = document.querySelector("#resume-button-1");
+    resume_btn2.addEventListener("click", (e) => {
+      e.preventDefault();
+      window.location.href = "resume/Konark-Kumar-Resume.pdf";
+    });
+  });
 document.addEventListener('DOMContentLoaded', function() {
     const resume_btn2 = document.querySelector("#resume-button-2");
     resume_btn2.addEventListener("click", (e) => {
