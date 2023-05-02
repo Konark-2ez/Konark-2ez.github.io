7c2252760c1580cc8686d6f95f701aa0f57c4ca7
Konark-2ez <konarkkumar456@gmail.com>
2023-05-03 01:24:00 +0530

    one last change

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const resume_btn1 = document.getElementById("resume-button-1")
 const resume_btn2 = document.getElementById("resume-button-2")
+const resume_btn3 = document.getElementById("resume-button")
 
 resume_btn1.addEventListener("click", (e) => {
   e.preventDefault();
@@ -18,6 +19,14 @@ resume_btn2.addEventListener("click", (e) => {
   window.location.href =
     "https://drive.google.com/file/d/1GfU7MINpsxg1hhx5D58duW0aaOvVOCog/view?usp=share_link";
 });
+resume_btn3.addEventListener("click", (e) => {
+    e.preventDefault();
+    window.open(
+      "https://drive.google.com/file/d/1GfU7MINpsxg1hhx5D58duW0aaOvVOCog/view?usp=share_link"
+    );
+    window.location.href =
+      "https://drive.google.com/file/d/1GfU7MINpsxg1hhx5D58duW0aaOvVOCog/view?usp=share_link";
+  });
 document.addEventListener('DOMContentLoaded', function() {
     const resume_btn2 = document.querySelector("#resume-button-2");
     resume_btn2.addEventListener("click", (e) => {
