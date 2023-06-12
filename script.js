f4ad6ee9238b3d003bb19c1ce432eaf6208c9539
Konark-2ez <konarkkumar456@gmail.com>
2023-06-12 21:00:35 +0530

    made required changes

--- script.js
+++ script.js
@@ -1,43 +1,20 @@
-const resume_btn1 = document.getElementById("resume-button-1")
-const resume_btn2 = document.getElementById("resume-button-2")
-const resume_btn3 = document.getElementById("resume-button")
+const resume_btn1 = document.querySelector("#resume-button-1");
+const resume_btn2 = document.querySelector("#resume-button-2");
 
 resume_btn1.addEventListener("click", (e) => {
   e.preventDefault();
   window.open(
-   "https://drive.google.com/file/d/1GfU7MINpsxg1hhx5D58duW0aaOvVOCog/view?usp=share_link"
+    "https://drive.google.com/file/d/1GfU7MINpsxg1hhx5D58duW0aaOvVOCog/view?usp=sharing"
   );
   window.location.href =
-    "https://drive.google.com/file/d/1GfU7MINpsxg1hhx5D58duW0aaOvVOCog/view?usp=share_link";
+    "https://drive.google.com/uc?export=download&id=1GfU7MINpsxg1hhx5D58duW0aaOvVOCog";
 });
 
 resume_btn2.addEventListener("click", (e) => {
   e.preventDefault();
   window.open(
-    "https://drive.google.com/file/d/1GfU7MINpsxg1hhx5D58duW0aaOvVOCog/view?usp=share_link"
+    "https://drive.google.com/file/d/1GfU7MINpsxg1hhx5D58duW0aaOvVOCog/view?usp=sharing"
   );
   window.location.href =
-    "https://drive.google.com/file/d/1GfU7MINpsxg1hhx5D58duW0aaOvVOCog/view?usp=share_link";
+    "https://drive.google.com/uc?export=download&id=1GfU7MINpsxg1hhx5D58duW0aaOvVOCog";
 });
-resume_btn3.addEventListener("click", (e) => {
-    e.preventDefault();
-    window.open(
-      "https://drive.google.com/file/d/1GfU7MINpsxg1hhx5D58duW0aaOvVOCog/view?usp=share_link"
-    );
-    window.location.href =
-      "https://drive.google.com/file/d/1GfU7MINpsxg1hhx5D58duW0aaOvVOCog/view?usp=share_link";
-  });
-  document.addEventListener('DOMContentLoaded', function() {
-    const resume_btn2 = document.querySelector("#resume-button-1");
-    resume_btn2.addEventListener("click", (e) => {
-      e.preventDefault();
-      window.location.href = "resume/Konark-Kumar-Resume.pdf";
-    });
-  });
-document.addEventListener('DOMContentLoaded', function() {
-    const resume_btn2 = document.querySelector("#resume-button-2");
-    resume_btn2.addEventListener("click", (e) => {
-      e.preventDefault();
-      window.location.href = "resume/Konark-Kumar-Resume.pdf";
-    });
-  });
